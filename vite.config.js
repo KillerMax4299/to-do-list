dce966a25b3e7f1f9d273d529ea888d9c4c990cd
killermax <thekillermax1999@gmail.com>
2023-11-30 22:44:30 +0530

    pwa config added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "To do List",
+        short_name: "To do List",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#fff",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "/vite.svg",
+            sizes: "any",
+            type: "svg",
+            purpose: "any maskable",
+          },
+          
+        ],
+        theme_color: "#a6a6a6",
+      },
+      
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+    }),
+  ],
+});
